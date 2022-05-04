c7fbbdbbc4712ad3defc9e59f271bcb7274e13f9
JakkalavadikiSruthi <103100661+JakkalavadikiSruthi@users.noreply.github.com>
2022-05-04 14:30:22 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsComponent } from './products/products.component';
+import { Routes, RouterModule } from '@angular/router';
+import { reducer } from './store/reducer';
+import { StoreModule } from '@ngrx/store';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/products',
+    pathMatch: 'full'
+  },
+  {
+      path: 'products',
+      component: ProductsComponent
+  },
+  {
+      path: 'cart',
+      component: CartComponent
+  },
+  {
+      path: 'product/:id',
+      component: ProductComponent
+  },
+  {
+      path: '**',
+      redirectTo: '',
+      pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ProductComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    StoreModule.forRoot({cart: reducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
